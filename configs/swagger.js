5096ec5af0a4029caf02a87a9ba85a4e36ca3b80
Josue Garcia <jgarcia-2023324@kinal.edu.gt>
2025-02-12 00:09:02 -0600

    add: se agrego estructura del proyecto.

---
+++ configs/swagger.js
@@ -0,0 +1,28 @@
+import { version } from "mongoose";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express"
+const swaggerOptions = {
+    swaggerDefinition:{
+        openapi: "3.0.0",
+        info:{
+            title: "sales Systems API",
+            version:"1.0.0",
+            description: "API para gestion de ventas.",
+            contact:{
+                name: "Josue Garcia",
+                email: "jgarcia-2023324@kinal.org.gt"
+            }
+        },
+        servers:[
+            {
+                url: "http://127.0.0.1:3001/salesManager/v1"
+            }
+        ]
+    },
+    apis:[
+        "./src/auth/*.js",
+        "./src/user/*.js",
+    ]
+}
+const swaggerDocs = swaggerJSDoc(swaggerOptions)
+export { swaggerDocs, swaggerUi }
