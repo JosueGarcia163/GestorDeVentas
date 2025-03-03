7b7e07d64f10bbc2268b3969669ec6fb1755bc0b
Josue Garcia <jgarcia-2023324@kinal.edu.gt>
2025-03-03 16:07:12 -0600

    feat: se agrego funcionalidades de producto.

--- src/product/product.controller.js
+++ src/product/product.controller.js
@@ -67,6 +67,7 @@ export const getProductById = async (req, res) => {
             //Buscamos el atributo name y description por medio del campo que hace referenca a categorias dentro de publicacion
             .populate("category", "name description")
 
+            //Envio un 200 para dar un ok.
         return res.status(200).json({
             success: true,
             product
@@ -79,6 +80,150 @@ export const getProductById = async (req, res) => {
         })
     }
 }
+
+export const getProductOutStock = async (req, res) => {
+    try {
+        const products = await Product.find({ stock: 0 })
+
+            //Buscamos el atributo name y description por medio del campo que hace referenca a categorias dentro de publicacion
+            .populate("category", "name description")
+
+            //Si todo funciona enviamos un 200 o ok.
+        return res.status(200).json({
+            success: true,
+            products
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Error al obtener los productos agotados.",
+            error: err.message
+        })
+    }
+}
+
+
+export const getProductMoreSeller = async (req, res) => {
+    try {
+        const products = await Product.find()
+            .sort({ soldQuantity: -1 })
+            //Buscamos el atributo name y description por medio del campo que hace referenca a categorias dentro de publicacion
+            .populate("category", "name description")
+
+            //Si todo funciona enviamos un 200 o ok.
+        return res.status(200).json({
+            success: true,
+            products
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "Error al obtener los productos mas vendidos.",
+            error: err.message
+        })
+    }
+}
+
+
+export const updateProduct = async (req, res) => {
+    try {
+        //extraemos el id del params
+        const { id } = req.params;
+        // desestructuramos los campos del body
+        const { name, stock, text, price, category } = req.body;
+        
+        //Buscamos un producto por medio del id del params
+        const product = await Product.findById(id)
+
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                msg: "Producto no encontrada",
+            });
+        }
+
+        //Busco la categoria por nombre y la guardo en "existingCategory" para poder actualizarla
+        const existingCategory = await Category.findOne({ name: category })
+        if (!existingCategory) {
+            return res.status(404).json({ message: "No se encontro categoria en la db, es necesario colocar la categoria" });
+        }
+
+        //Actualizo los parametros dentro de la base de datos. 
+        product.name = name || product.name;
+        product.stock = stock || product.stock;
+        product.text = text || product.text;
+        product.price = price || product.price;
+        product.category = existingCategory._id;
+
+        //Guardo el producto en la base de datos
+        await product.save();
+
+        res.status(200).json({
+            success: true,
+            msg: 'Producto Actualizado',
+            product,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            msg: "Error al actualizar el producto",
+            error,
+        });
+    }
+}
+
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await Product.findById(id)
+
+        //validamos que la publicacion no venga vacia.
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: "Producto no encontrado"
+            });
+        }
+
+        //Validamos para ver si la publicacion ya esta eliminada.
+        if (product.status == "false") {
+            return res.status(400).json({
+                success: false,
+                message: "El producto ya ha sido eliminada previamente."
+            })
+        }
+
+        const updateProduct = await Product.findByIdAndUpdate(
+            id,
+            { status: false },
+            { new: true }
+        )
+        return res.status(200).json({
+            success: true,
+            message: "Producto eliminado.",
+            product: updateProduct
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            msg: "Error al eliminar le producto",
+            error,
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
 
 
 
--- src/product/product.routes.js
+++ src/product/product.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
-import { createProduct, getProduct, getProductById } from "./product.controller.js"
-import { createValidator, getProductByIdValidator, getProductValidator } from "../middlewares/product-validators.js"
+import { createProduct, getProduct, getProductById, getProductOutStock, getProductMoreSeller, updateProduct, deleteProduct } from "./product.controller.js"
+import { createValidator, getProductByIdValidator, getProductValidator, updateProductValidator, deleteProductValidator } from "../middlewares/product-validators.js"
 
 const router = Router()
 
@@ -10,6 +10,19 @@ router.post("/createProduct", createValidator, createProduct)
 router.get("/getProduct", getProductValidator, getProduct)
 
 router.get("/getProductById/:id", getProductByIdValidator, getProductById)
+
+router.get("/getProductOutOfStock", getProductValidator, getProductOutStock)
+
+router.get("/getProductMoreSeller", getProductValidator, getProductMoreSeller)
+
+router.put("/updateProduct/:id", updateProductValidator, updateProduct)
+
+router.delete("/deleteProduct/:id", deleteProductValidator, deleteProduct)
+
+
+
+
+
 
 export default router
 
